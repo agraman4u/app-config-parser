0b29953116eae50f7fb2b0f9c36abca511a88c16
amaagra <agraman4u@gmail.com>
2025-07-05 21:42:24 +0530

    Re-enable artifact signing

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -75,7 +75,7 @@ android {
 mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-//    signAllPublications()
+    signAllPublications()
 
     coordinates(group.toString(), "app-config-parser", version.toString())
 
